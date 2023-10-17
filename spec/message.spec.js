6941d7fa4519b08e0795a2e5596b24a885105775
Arielle Waks <ariellewaks@Arielles-Air.lan>
2023-10-17 16:24:27 -0500

    TDD tests written for Message class

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,22 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+    test('throws error if a name is NOT passed into the constructor as the first parameter', function () {
+        expect( function() {
+            new Message();
+           })
+         .toThrow(new Error('Message name required.'));
+    });
+
+    test('constructor sets name', function () {
+        let messageNameTest = new Message ('Test message name', commands);
+        expect (messageNameTest.name).toBe('Test message name')
+    });
+
+    test('contains a commands array passed into the constructor as the 2nd argument', function () {
+        let commandArray = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+        let messageCommandTest = new Message ('Test message array', commandArray);
+        expect (messageCommandTest.commands).toBe(commandArray)
+    });
 
 });
